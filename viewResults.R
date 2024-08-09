a15ca2310eb2c3972d3fb8d2b5579235ee7c422f
egillax <egillax@gmail.com>
2024-08-09 12:32:47 +0200

    fix model, add new validation json and result viewing

---
+++ viewResults.R
@@ -0,0 +1,7 @@
+db <- "~/ohdsi-studies/PandemicPrediction/results/new_validation/strategusWork/PatientLevelPredictionValidationModule_3/sqlite/databaseFile.sqlite"
+connectionDetailsInput <- list(dbms = "sqlite", server=db)
+databaseSettings <- list(connectionDetailSettings = connectionDetailsInput,
+                         schema = "main")
+
+PatientLevelPrediction:::viewPlps(databaseSettings)
+
