58a2a1f818197f6204ea1db9c9c68e7abc85b2fa
egillax <egillax@gmail.com>
2024-10-08 14:09:29 +0200

    save cohorts in repo for reproducibility

--- extras/codeToCreateJsons.R
+++ extras/codeToCreateJsons.R
@@ -6,7 +6,7 @@ library(lubridate)
 
 githubSettings <- data.frame(user = "ohdsi-studies",
                              repository = "PandemicPrediction",
-                             ref = "master",
+                             ref = "strategus_v1",
                              modelsFolder = "models")
 
 modelTransferModule <- ModelTransferModule$new()
@@ -16,37 +16,14 @@ modelTransferModuleSpecs <- modelTransferModule$createModuleSpecifications(githu
 
 # COHORT GENERATOR MODULE ------------------------------------------------------
 
-# atlas Id's from my personal atlas ~ Egill
-# maybe better for reproducibility to have the jsons in the repo and use them
-cohortIds <- list(outpatientVisit =  12,
-                  inPatientVisit = 25,
-                  death = 11,
-                  severe = 14,
-                  critical = 13,
-                  cancer = 15,
-                  copd = 16,
-                  diabetes = 17,
-                  heartDisease = 18,
-                  hyperlipidemia = 19,
-                  hypertension = 20,
-                  kidneyDisease  = 21,
-                  ulcer = 22,
-                  hepaticFailure = 23,
-                  respFailure = 24)
-
-baseUrl <- keyring::key_get('webapi', 'baseurl')
-ROhdsiWebApi::authorizeWebApi(
-  baseUrl = baseUrl,
-  authMethod = 'db',
-  webApiUsername = keyring::key_get('webapi', 'username'),
-  webApiPassword = keyring::key_get('webapi', 'password')
+cohortDefinitions <- CohortGenerator::getCohortDefinitionSet(
+  settingsFileName = 'inst/Cohorts.csv',
+  jsonFolder = "inst/cohorts",
+  sqlFolder = "inst/sql/sql_server",
+  cohortFileNameFormat = "%s_%s",
+  cohortFileNameValue = c("cohortId", "cohortName")
 )
 
-cohortDefinitions <- ROhdsiWebApi::exportCohortDefinitionSet(
-  baseUrl = baseUrl,
-  cohortIds = unlist(cohortIds),
-  generateStats = TRUE
-)
 # modify the cohort
 cohortGeneratorModule <- CohortGeneratorModule$new()
 cohortDefShared <- cohortGeneratorModule$createCohortSharedResourceSpecifications(cohortDefinitions)
