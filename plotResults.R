56a5d3b2e9abb4ec5276b2d8c6240c07f539e269
Admin_EFridgei <Admin_EFridgei@its.jnj.com>
2024-11-07 03:25:04 -0500

    make plot work for more metrics, and extract pop and outcome sizes

--- plotResults.R
+++ plotResults.R
@@ -7,12 +7,33 @@ library(ggplot2)
 extractPerformance <- function(analysisPath) {
   analysis <- readRDS(file.path(analysisPath, "validationResult", "runPlp.rds"))
   perfMetric <- analysis$performanceEvaluation$evaluationStatistics |>
-    dplyr::filter(.data$metric == "AUROC", .data$evaluation == "Validation") |>
-    dplyr::pull(.data$value)
-  if (inherits(perfMetric, "list")) {
-    perfMetric <- perfMetric[[1]]
-  }
+    dplyr::filter(.data$metric %in% c("AUROC", "populationSize", "outcomeCount",
+                                    "Eavg"),
+                  .data$evaluation == "Validation") |>
+    dplyr::select("metric", "value") |>
+    tidyr::pivot_wider(names_from="metric", values_from="value") |>
+    dplyr::mutate(populationSize = as.integer(.data$populationSize),
+                  outcomeCount = as.integer(.data$outcomeCount),
+                  Eavg = as.numeric(.data$Eavg),
+                  AUROC = as.numeric(.data$AUROC))
   return(perfMetric)
+}
+
+getModelName <- function(task) {
+  if (task$targetId == 12) {
+    if (task$outcomeId == 13) {
+      modelName = "dataDrivenI"
+      return(modelName)
+    } else if (task$outcomeId == 11) {
+      modelName = "dataDrivenF"
+      return(modelName) 
+    } else if (task$outcomeId == 14) {
+      modelName = "dataDrivenH"
+      return(modelName)
+    } else {
+      stop(paste0("Unknown outcome id: ", task$outcomeId))
+    }
+  }
 }
 
 extractModelInfo <- function(analysisPath) {
@@ -25,29 +46,30 @@ extractModelInfo <- function(analysisPath) {
                      endDate = as.Date(endDate, format = "%Y%m%d"))
   task <- list(targetId = validationDetails$targetId, outcomeId = validationDetails$outcomeId)
   modelName <- attr(modelDesign$modelSettings$param, "settings")$name
-  modelInfo <- list(name = modelName, task = task, timePeriod = timePeriod)
+  if (modelName == "Existing GLM") {
+    modelName <- getModelName(task)
+  }
+  modelInfo <- data.frame(name = modelName, 
+                          targetId = task$targetId, 
+                          outcomeId=task$outcomeId,
+                          startDate = timePeriod$startDate,
+                          endDate = timePeriod$endDate)
   return(modelInfo)
 }
 
-resultFolder <- './results/strategus_new/strategusWork/PatientLevelPredictionValidationModule/ducky/'
+resultFolder <- './results/strategus_v1/strategusWork/PatientLevelPredictionValidationModule/OPTUM Extended DOD/'
 
 analyses <- dir(resultFolder, pattern = "Analysis*")
 
 
-performance <- list()
-modelInfo <- list()
+allPerformance <- data.frame()
 for (i in seq_along(analyses)) {
   analysisPath <- file.path(resultFolder, analyses[[i]])
-  performance[[i]] <- extractPerformance(analysisPath)
-  modelInfo[[i]] <- extractModelInfo(analysisPath)
+  performance <- extractPerformance(analysisPath)
+  modelInfo <- extractModelInfo(analysisPath)
+  combined <- cbind(modelInfo, performance)
+  allPerformance <- rbind(allPerformance, combined)
 }
-
-results <- data.frame(performance = unlist(performance),
-                      name = sapply(modelInfo, function(x) x$name),
-                      start_date = sapply(modelInfo, function(x) x$timePeriod$startDate),
-                      end_date = sapply(modelInfo, function(x) x$timePeriod$endDate),
-                      target = sapply(modelInfo, function(x) x$task$targetId),
-                      outcome = sapply(modelInfo, function(x) x$task$outcomeId))
 
 getName <- function(target, outcome) {
   firstName <- switch(as.character(target),
@@ -58,22 +80,24 @@ getName <- function(target, outcome) {
                        "11" = "Death",
                        "13" = "Critical",
                        "14" = "Hospital",
-                       "Unknown target")
+                       "Unknown Outcome")
   name <- paste0(firstName, secondName)
   return(name)
 }
 
-results <- results |>
-  dplyr::mutate(
-                start_date = as.Date(.data$start_date),
-                end_date = as.Date(.data$end_date)) |>
-  dplyr::arrange(.data$start_date)
+results <- allPerformance |>
+  dplyr::mutate(taskName = mapply(getName, .data$targetId, .data$outcomeId)) |>
+  dplyr::arrange(.data$startDate)
 
+selectedModels <- c("coverH", "dataDrivenH")
+metric <- "AUROC"
+results <- results |> dplyr::filter(.data$name %in% selectedModels) |> 
+  dplyr::select(dplyr::all_of(metric) , "startDate", "endDate", "name", "taskName")
 
-ggplot(results, aes(x = start_date, y = performance, colour = name)) +
-  geom_segment(aes(xend = end_date, yend = performance), colour = "black") +
+ggplot(results, aes(x = .data$startDate, y = .data[[metric]], colour = .data$name)) +
+  geom_segment(aes(xend = .data$endDate, yend = .data[[metric]]), colour = "black") +
   geom_point(size = 3) +
-  geom_point(aes(x = end_date), size = 3) +
+  geom_point(aes(x = .data$endDate), size = 3) +
   theme_bw() + 
   scale_x_date(date_breaks = "1 month", date_labels = "%Y-%m", minor_breaks = NULL) +
   theme(legend.position = "right",
