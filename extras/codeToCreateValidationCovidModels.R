f29be5843a95f1c30bd4c700254dd3c2df18a741
egillax <egillax@gmail.com>
2025-08-30 00:05:51 +0200

    fix covid validation creation

--- extras/codeToCreateValidationCovidModels.R
+++ extras/codeToCreateValidationCovidModels.R
@@ -2,18 +2,16 @@ library(dplyr)
 library(stringr)
 library(lubridate)
 library(purrr)
+library(PatientLevelPrediction)
+library(Strategus)
 
 newModelsDir <- "./inst/newModels/"
-package <- "PandemicPrediction"
+packageName <- "PandemicPrediction"
+overallEndDate <- "2023-06-01"
 
 createPackageModel <- function(modelFolder, package) {
-  result <- list(
-    type = "package",
-    modelFolder = modelFolder,
-    package = package
-  )
+  result <- list(type = "package", modelFolder = modelFolder, package = package)
   class(result) <- "plpModel"
-
   return(result)
 }
 
@@ -21,55 +19,58 @@ getOutcomeId <- function(outcomeName) {
   switch(outcomeName,
     "Death" = 11,
     "Critical" = 13,
-    "Hospital" = 14,
-    NA
+    "Hospital" = 14
   )
 }
 
 getTargetId <- function(outcomeName) {
   return(31)
 }
-# Scans the models directory and parses folder names to extract metadata.
+
 discoverAndParseModels <- function(modelsDir) {
-  modelFolder <- list.dirs(modelsDir, full.names = TRUE, recursive = FALSE)
-  if (length(modelFolder) == 0) {
+  modelFolders <- list.dirs(modelsDir, full.names = TRUE, recursive = FALSE)
+  if (length(modelFolders) == 0) {
     warning("No model folders found in: ", modelsDir)
-    return(tibble())
+    return(tibble::tibble())
   }
-  message("Found ", length(modelFolder), " models to process for validation design.")
-  parsedModels <- map_dfr(modelFolder, function(folderPath) {
+
+  message("Found ", length(modelFolders), " models to process for validation design.")
+
+  parsedModels <- purrr::map_dfr(modelFolders, function(folderPath) {
     folderName <- basename(folderPath)
-    matches <- str_match(folderName, "^([^_]+)_([^_]+)_(\\d{8})_to_(\\d{8})$")
+
+    regexPattern <- "^([^_]+)_([^_]+)_([^_]+_)?(\\d{8})_to_(\\d{8})$"
+    matches <- stringr::str_match(folderName, regexPattern)
+
     if (is.na(matches[1, 1])) {
       warning("Folder name does not match expected format and will be skipped: ", folderName)
       return(NULL)
     }
-    tibble(
+
+    outcomeName <- matches[1, 2]
+    featureSet <- matches[1, 3]
+    devStartDate <- lubridate::ymd(matches[1, 5])
+    devEndDate <- lubridate::ymd(matches[1, 6])
+
+    tibble::tibble(
       path = folderPath,
-      outcomeName = matches[1, 2],
-      featureSet = matches[1, 3],
-      devStartDate = ymd(matches[1, 4]),
-      devEndDate = ymd(matches[1, 5]),
-      targetId = getTargetId(matches[1, 2]),
-      outcomeId = getOutcomeId(matches[1, 2])
+      outcomeName = outcomeName,
+      featureSet = featureSet,
+      devStartDate = devStartDate,
+      devEndDate = devEndDate,
+      targetId = getTargetId(outcomeName),
+      outcomeId = getOutcomeId(outcomeName)
     )
   })
+
   if (any(is.na(parsedModels$outcomeId))) {
     warning("Some models had outcome names that could not be mapped to an ID and will be excluded.")
-    parsedModels <- parsedModels %>% filter(!is.na(.data$outcomeId))
+    parsedModels <- parsedModels |> dplyr::filter(!is.na(.data$outcomeId))
   }
+
   return(parsedModels)
 }
 
-
-newModelDetails <- discoverAndParseModels(modelsDir = newModelsDir)
-cat("\n--- Discovered and Parsed Model Details ---\n")
-print(newModelDetails)
-cat("\n")
-
-overallEndDate <- "2023-06-01"
-
-# Generates a list of rolling 3-month RestrictPlpDataSettings objects
 setDynamicRestrictSettings <- function(validationStartDate, overallEndDate, interval = months(3)) {
   startDate <- ymd(validationStartDate)
   endDate <- ymd(overallEndDate)
@@ -96,40 +97,44 @@ setDynamicRestrictSettings <- function(validationStartDate, overallEndDate, inte
 }
 
 createValidationDesigns <- function(modelDetails) {
-  validationDesigns <- pmap(modelDetails, function(path, outcomeId, targetId, devEndDate, ...) {
-    validationStartDate <- devEndDate + days(1)
-    message(sprintf("Creating validation design for model '%s', starting %s", basename(path), validationStartDate))
+  validationDesigns <- purrr::pmap(modelDetails, function(path, outcomeId, targetId, devEndDate, ...) {
+    validationStartDate <- devEndDate + lubridate::days(1)
+
     restrictPlpDataSettingsList <- setDynamicRestrictSettings(
       validationStartDate = validationStartDate,
       overallEndDate = overallEndDate
     )
+
     if (length(restrictPlpDataSettingsList) == 0) {
-      warning("No validation windows for model ", basename(path), ". It will not be included in the analysis.")
       return(NULL)
     }
+
     relativeModelPath <- stringr::str_remove(path, "^./inst/")
     packageModel <- createPackageModel(
       modelFolder = relativeModelPath,
-      package = package
+      package = packageName
     )
-    validationDesign <- PatientLevelPrediction::createValidationDesign(
+
+    PatientLevelPrediction::createValidationDesign(
       targetId = targetId,
       outcomeId = outcomeId,
-      populationSettings = NULL, # Use settings from the trained model
+      populationSettings = NULL,
       restrictPlpDataSettings = restrictPlpDataSettingsList,
       plpModelList = list(packageModel),
       recalibrate = "weakRecalibration",
       runCovariateSummary = TRUE
     )
-    return(validationDesign)
   })
-  validationDesigns <- purrr::list_flatten(validationDesigns)
 
-  validationDesigns <- compact(validationDesigns)
-  return(validationDesigns)
+  validationDesignsFlat <- purrr::list_flatten(validationDesigns)
+
+  return(purrr::compact(validationDesignsFlat))
 }
 
-# --- Execute the validation design creation ---
+newModelDetails <- discoverAndParseModels(modelsDir = newModelsDir)
+cat("\n--- Discovered and Parsed Model Details ---\n")
+print(newModelDetails, n = Inf)
+
 validationList <- createValidationDesigns(modelDetails = newModelDetails)
 cat("\n--- Successfully created", length(validationList), "validation designs ---\n")
 
@@ -141,7 +146,7 @@ cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSet(
   cohortFileNameValue = c("cohortId", "cohortName")
 )
 cohortDefinitionSet <- cohortDefinitionSet |>
-  dplyr::filter(.data$cohortId != 30) # only used for development
+  dplyr::filter(.data$cohortId != 30)
 
 cohortGeneratorModule <- Strategus::CohortGeneratorModule$new()
 cohortDefinitionShared <- cohortGeneratorModule$createCohortSharedResourceSpecifications(cohortDefinitionSet)
@@ -157,11 +162,10 @@ analysisSpecifications <- Strategus::createEmptyAnalysisSpecificiations() |>
   Strategus::addModuleSpecifications(cohortGeneratorModuleSpecifications) |>
   Strategus::addModuleSpecifications(predictionValidationModuleSpecifications)
 
-# --- Save the final JSON object ---
 outputFileName <- "./inst/study_execution_jsons/new_models_validation.json"
 ParallelLogger::saveSettingsToJson(
   object = analysisSpecifications,
   fileName = outputFileName
 )
 
-message("\nAnalysis specifications for new models successfully saved to:\n", normalizePath(outputFileName))
+message("\nAnalysis specifications for all new models successfully saved to:\n", normalizePath(outputFileName))
