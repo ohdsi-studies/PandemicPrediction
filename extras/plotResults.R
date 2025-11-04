e585781e2336842dcf8682db669fa0322f168355
egillax <egillax@gmail.com>
2025-11-04 11:33:57 +0100

    clean jsons and create new validation

--- extras/plotResults.R
+++ extras/plotResults.R
@@ -12,21 +12,41 @@ allResults <- dplyr::bind_rows(
     analysisId = "val_original"
   ),
   getAnalysisResults(
-    databasePath = "results/new-model-validation/databaseFile.sqlite",
+    databasePath = "results/covid-validation/databaseFile.sqlite",
     evaluationType = "Validation",
     analysisId = "val_new"
+  ),
+  getAnalysisResults(
+    databasePath = "results/recalibrated-validation/databaseFile.sqlite",
+    evaluationType = "Validation",
+    analysisId = "val_new_recalibrated"
   )
 )
+
+val <- dplyr::filter(allResults, evaluationType == "val_original")
+recal <- dplyr::filter(allResults, evaluationType == "val_new_recalibrated")
 
 print("--- Master Results Data Frame ---")
 print(dplyr::glimpse(allResults))
 
-
-plotComparison(
+plot <- plotComparison(
   allResults = allResults,
-  outcomes = c("Death", "Critical", "Hospital"),
-  modelOriginsToCompare = c("Original Influenza", "New Covid"),
+  outcomes = c("Fatality", "Hospitalization", "RespiratoryFailure"),
+  modelOriginsToCompare = c("Original Influenza"),
   featureSetsToCompare = c("Full", "Parsimonious"),
-  devPeriodsToCompare = c("First 6 Months"),
-  facetBy = c("featureSet", "outcomeName"),
+  devPeriodsToCompare = c("First 9 Months"),
+  facetBy = c("outcomeName"),
+  metric = "Eavg",
+  title = "Calibration performance",
+  showOutcomeRatePanel = TRUE,            # turn on Option C
+  outcomeRatePanelStyle = "area"          # or "bars"
+)
+
+ggplot2::ggsave(
+  filename = "results/figures/calibration_performance.svg",
+  plot = plot,
+  width = 380,
+  height = 250,
+  units = "mm",
+  dpi = 300
 )
